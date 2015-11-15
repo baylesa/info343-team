b859bfbf2a7b4399021b9a2f02fceac3683d95ff
esc11 <esc11@uw.edu>
2015-11-15 00:03:24 -0800

    updated protractor file for last name field

--- test/signup-spec.js
+++ test/signup-spec.js
@@ -3,6 +3,19 @@ describe('Team Challenge App', function() {
   it('should have a title', function() {
     browser.get('https://github.com/baylesa/info343-team');
 
-    expect(browser.getTitle()).toEqual('Info Team');
+    expect(browser.getTitle()).toEqual('baylesa/info343-team');
   });
 });
+
+describe('Last Name Field', function() {
+    it('should display error message if empty and touched', function(){
+      browser.get('http://localhost:8000/');
+
+      var lastNameBar = element(by.model('last'));
+      lastNameBar.sendKeys('McGee');
+      lastNameBar.clear();
+
+      var errorMessage = element(by.css('.help-block'));
+      expect( errorMessage.isPresent() ).toEqual(true);
+    });
+})
