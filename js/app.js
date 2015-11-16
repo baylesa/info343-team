3bc5a68db0caceab7874b4c7d5f7cc37bb4d77df
Joseph Lee <yoseb123@hotmail.com>
2015-11-15 16:54:41 -0800

    changed age

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ angular.module('TeamChallengeApp', ['ngSanitize'] ) //ngSanitize for HTML displa
 // age restriction 
 var agePrompt=prompt("What is your age?");
 
-if (agePrompt>=18) alert('Welcome!')
+if (agePrompt>=13) alert('Welcome!')
 else {
 alert('Sorry! Must be at least 13 to sign up')
 document.location="https://www.youtube.com/watch?v=YaG5SAw1n0c";//link if not 18
